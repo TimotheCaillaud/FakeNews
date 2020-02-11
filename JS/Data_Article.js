3fe71d1a34da756afa373e549bcbb584c9fffccd
AntoninCheymoll <antonin.cheymol@u-psud.fr>
2020-02-11 23:47:06 +0100

    apparence des publications

--- JS/Data_Article.js
+++ JS/Data_Article.js
@@ -2,31 +2,44 @@ let Data_Article = [
     Article_1={
         Publieur: "Complot Facile",
         Photo: "contact_android.png",
-        Titre: "Ce que le gouvernement vous cache sur les pains aux chocolats !",
-        Contenu: "En fait ce sont des CHOCOLATINES !! OMG ..."
+        Image: "coronavirus.jpg",
+        Contenu: "En fait ce sont des CHOCOLATINES !! OMG ...",
+        Likes: 27,
+        Comments: 10,
+        Shares: 2
     },
     Article_2={
         Publieur: "La verite",
         Photo: "contact_android.png",
-        Titre: "On va tous mourir, la raison va vous surprendre",
-        Contenu: "On est pas immortel connard !"
+        Image: "coronavirus.jpg",
+        Link: "http://www.charismamag.com/blogs/fire-in-my-bones/36789-north-korea-will-open-its-doors-to-christianity",
+        Contenu: "On est pas immortel connard !",
+        Likes: 27,
+        Comments: 10,
+        Shares: 2
     },
     Article_3={
         Publieur: "Les vrais sachent",
         Photo: "contact_android.png",
-        Titre: "Sachiez vous comment sachoir ?",
-        Contenu: "Il vous suffit de sacher"
+        Image: "coronavirus.jpg",
+        Contenu: "Il vous suffit de sacher",
+        Likes: 27,
+        Comments: 10,
+        Shares: 2
     },
     Article_4={
         Publieur: "Trop fake",
         Photo: "contact_android.png",
-        Titre: "Je vous dit la verité sur moi !",
-        Contenu: "GET PRANKED !"
+        Image: "coronavirus.jpg",
+        Contenu: "GET PRANKED !",
+        Likes: 27,
+        Comments: 10,
+        Shares: 2
     }
 ];
 
 function chargementDesArticles(){
-    
+
     document.write(
         '<div id="Gauche" class="d-inline-block"></div>'+
         '<div id="Liste_Articles" class="bg-warning px-5 d-inline-block">'
@@ -36,13 +49,27 @@ function chargementDesArticles(){
             document.write(
             '<div class="Article mx-5 my-3 bg-primary border">'+
                 '<div class="Article_Header d-flex flex-row border-bottom">'+
-                    '<div class="d-inline-block">'+
-                        '<img src="../'+ Data_Article[j].Photo +'" class="Photo_posteur" />'+
+                    '<div class=" d-inline-block">'+
+                      '<img src="../ImageSources/'+ Data_Article[j].Photo +'" class="Photo_posteur" />'+
                     '</div>'+
-                    '<div class="Titre_Article d-inline-block">'+ Data_Article[j].Titre +
+                    '<div class="Publieur ">'+ Data_Article[j].Publieur +
                     '</div>'+
                     '</div>'+
-                '<p>' + Data_Article[j].Contenu + '</p>'+
+                    '<div class="Contenu ">'+ Data_Article[j].Contenu +
+                    '</div>'+
+
+                    '<div class="Image">'+
+                      '<img src="../ImageSources/'+ Data_Article[j].Image + ' ""/>'+
+                    '</div>'+
+
+                    '<div class= "d-flex Publication_Statistiques ">'+
+                      '<img class="Stat_Logo" src="../ImageSources/like_logo.svg" class="Image" />'+
+                      '<div class="Stat_Num">'+ Data_Article[j].Likes +'</div>'+
+                      '<img class="Stat_Logo" src="../ImageSources/share_logo.png" class="Image" />'+
+                      '<div class="Stat_Num">' + Data_Article[j].Comments + '</div>'+
+                      '<img class="Stat_Logo" src="../ImageSources/comment_logo.png" class="Image" />'+
+                      '<div class="Stat_Num">' + Data_Article[j].Shares + '</div>'+
+                    '</div>'+
             '</div>'
             );
     }
--- JS/Data_Contact.js
+++ JS/Data_Contact.js
@@ -56,7 +56,7 @@ function chargementDesContacts() {
     for (var j = 0; j < Data_Contact.length; j++) {
         document.write(
             '<li class="nav-item d-flex flex-row">' +
-            '<img src="../' + Data_Contact[j].Photo + '" class="Photo_posteur" />' +
+            '<img src="../imageSources/' + Data_Contact[j].Photo + '" class="Photo_posteur" />' +
             '<a class="nav-link" data-toggle="tab" href="#' + Data_Contact[j].Ref + '">' + Data_Contact[j].Nom +
             '</a>' +
             '</li>'
@@ -145,7 +145,7 @@ function chargementDesContacts() {
                     '<button class="btn btn-outline-secondary" type="button" id="button-addon2">Send</button>' +
                 '</div>' +
             '</div>' +
-        '</div>' 
+        '</div>'
     );
 
     document.write(
