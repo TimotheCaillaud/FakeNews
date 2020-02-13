f7c1f997b29560145aa6fafdbe331c87df86808b
AntoninCheymoll <antonin.cheymol@u-psud.fr>
2020-02-13 05:00:05 +0100

    tab managing, profile picture and layut remanaging

--- JS/Data_Article.js
+++ JS/Data_Article.js
@@ -3,6 +3,7 @@ let Data_Article = [
         Publieur: "Complot Facile",
         Photo: "contact_android.png",
         Image: "coronavirus.jpg",
+        Link: "articleTest",
         Contenu: "En fait ce sont des CHOCOLATINES !! OMG ...",
         Likes: 27,
         Comments: 10,
@@ -12,29 +13,31 @@ let Data_Article = [
         Publieur: "La verite",
         Photo: "contact_android.png",
         Image: "coronavirus.jpg",
-        Link: "http://www.charismamag.com/blogs/fire-in-my-bones/36789-north-korea-will-open-its-doors-to-christianity",
+        Link: "articleTest",
         Contenu: "On est pas immortel connard !",
-        Likes: 27,
-        Comments: 10,
-        Shares: 2
+        Likes: 10,
+        Comments: 4,
+        Shares: 0
     },
     Article_3={
         Publieur: "Les vrais sachent",
         Photo: "contact_android.png",
         Image: "coronavirus.jpg",
+        Link: "articleTest",
         Contenu: "Il vous suffit de sacher",
-        Likes: 27,
-        Comments: 10,
+        Likes: 70,
+        Comments: 3,
         Shares: 2
     },
     Article_4={
         Publieur: "Trop fake",
         Photo: "contact_android.png",
         Image: "coronavirus.jpg",
+        Link: "articleTest",
         Contenu: "GET PRANKED !",
-        Likes: 27,
-        Comments: 10,
-        Shares: 2
+        Likes: 154,
+        Comments: 32,
+        Shares: 8
     }
 ];
 
@@ -58,16 +61,16 @@ function chargementDesArticles(){
                     '<div class="Contenu ">'+ Data_Article[j].Contenu +
                     '</div>'+
 
-                    '<div class="Image">'+
-                      '<img src="../ImageSources/'+ Data_Article[j].Image + ' ""/>'+
+                    '<div class="ArticleImagePreview">'+
+                      '<img  src="../ImageSources/'+ Data_Article[j].Image + '" onclick="addArticleWindow(\'' + Data_Article[j].Link + '\');" />'+
                     '</div>'+
 
                     '<div class= "d-flex Publication_Statistiques ">'+
                       '<img class="Stat_Logo" src="../ImageSources/like_logo.svg" class="Image" />'+
                       '<div class="Stat_Num">'+ Data_Article[j].Likes +'</div>'+
-                      '<img class="Stat_Logo" src="../ImageSources/share_logo.png" class="Image" />'+
-                      '<div class="Stat_Num">' + Data_Article[j].Comments + '</div>'+
                       '<img class="Stat_Logo" src="../ImageSources/comment_logo.png" class="Image" />'+
+                      '<div class="Stat_Num">' + Data_Article[j].Comments + '</div>'+
+                      '<img class="Stat_Logo" src="../ImageSources/share_logo.png" class="Image" />'+
                       '<div class="Stat_Num">' + Data_Article[j].Shares + '</div>'+
                     '</div>'+
             '</div>'
--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,3 @@
-
 document.getElementById("button-addon2").onclick = function () {
 
     var tmp = $('.nav-tabs .active').text();
@@ -24,20 +23,93 @@ document.getElementById("button-addon2").onclick = function () {
     document.getElementById('input_text').value = "";
 };
 
-let listOnglet = ["Corps", "Article_Premier"];
+
+function addWindows(article){
+  document.getElementById("button-addon2").onclick = function () {
+
+      var tmp = $('.nav-tabs .active').text();
+      var conv = "";
+
+      if (tmp == "") {
+          tmp = Data_Contact[0].Nom;
+      }
+      for (var j = 0; j < Data_Contact.length; j++) {
+          if (Data_Contact[j].Nom == tmp) {
+              conv = Data_Contact[j].Ref;
+          }
+      }
+
+      var ajout = document.getElementById('input_text').value;
+
+      if (ajout != "") {
+          document.getElementById("ajout_message_" + conv).innerHTML +=
+              '<div class="d-flex align-items-end flex-column"><div class="Text_Conv p-2 bd-highlight d-inline-flex px-3 mb-1 text-left bg-white rounded">' + ajout + '</div></div>';
+      }
+      console.log("ok" +  ajout);
+      ajout = "";
+      document.getElementById('input_text').value = "";
+  };
+}
+
+
+let listOnglet = ["FakeBook"];
 
 function changeVisibility(NameDiv) {
     listOnglet.forEach(element => {
         if (element == NameDiv) {
-            document.getElementById(element).classList.add('d-flex');
-            document.getElementById(element).classList.add('flex-row');
-            document.getElementById(element).classList.add('Page_Up');
-            document.getElementById(element).classList.remove('Page_Down');
+
+          document.getElementById(element).classList.add('d-flex');
+          document.getElementById(element).classList.add('flex-row');
+          document.getElementById(element).classList.add('Page_Up');
+          document.getElementById(element).classList.remove('Page_Down');
+          document.getElementById(element+"Trigger").classList.add('active');
+          if(document.getElementById(element+"Remove")){
+            document.getElementById(element+"Remove").src = "../ImageSources/removeMain.png";
+          }
+
         } else {
-            document.getElementById(element).classList.remove('d-flex');
-            document.getElementById(element).classList.remove('flex-row');
-            document.getElementById(element).classList.remove('Page_Up');
-            document.getElementById(element).classList.add('Page_Down');
+          document.getElementById(element).classList.remove('d-flex');
+          document.getElementById(element).classList.remove('flex-row');
+          document.getElementById(element).classList.remove('Page_Up');
+          document.getElementById(element).classList.add('Page_Down');
+          document.getElementById(element+"Trigger").classList.remove('active');
+          if(document.getElementById(element+"Remove")){
+            document.getElementById(element+"Remove").src = "../ImageSources/removeHidden.png";
+          }
         }
     });
 }
+
+function removeTab(tabName){
+  listOnglet.splice( listOnglet.indexOf(tabName), 1 );
+  document.getElementById(tabName +"Tab").remove();
+  document.getElementById(tabName).remove();
+  document.getElementById('FakeBookTrigger').click();
+}
+
+function addArticleWindow(articleTitle){
+  if(listOnglet.includes(articleTitle)){
+    document.getElementById(articleTitle + 'Trigger').click();
+    return;
+  }
+
+  listOnglet.push(articleTitle)
+  let navBar = document.getElementById("navBar");
+  navBar.innerHTML +=
+    '<li id="' + articleTitle +'Tab" class="nav-item">'+
+      '<a id="' + articleTitle +'Trigger" class="d-inline-block nav-link" onclick="changeVisibility(\'' + articleTitle + '\');" href="#' + articleTitle + '">' + articleTitle + '</a>'+
+      '<input id="' + articleTitle +'Remove" type="image" src="../ImageSources/removeMain.png"  onclick="removeTab(\'' + articleTitle + '\');" alt="Submit" width="10" height="10">' +
+    '</li>'
+
+
+  let site = document.getElementById("Total");
+  site.innerHTML +=
+   '<div class="ArticlePage" id="' + articleTitle + '" class="Page_Up d-flex flex-row">' +
+      '<img  src="../Articles/' + articleTitle + '.png"/>'+
+    '</div>'
+    
+
+  //console.log(site.innerHTML)
+  document.getElementById(articleTitle + 'Trigger').click();
+
+  }
