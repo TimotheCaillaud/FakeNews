ecb4a3988ad1752f4a85b259d46b515a845798c2
Timothé <timothe.caillaud@gmail.com>
2020-02-16 10:58:01 +0100

    Plus de problème

--- JS/main.js
+++ JS/main.js
@@ -60,7 +60,9 @@ function changeVisibility(NameDiv) {
 
           document.getElementById(element).classList.add('d-flex');
           document.getElementById(element).classList.add('flex-row');
-          document.getElementById(element).classList.add('Page_Up');
+          if(element != "FakeBook"){
+            document.getElementById(element).classList.add('Page_Up');
+          }
           document.getElementById(element).classList.remove('Page_Down');
           document.getElementById(element+"Trigger").classList.add('active');
           if(document.getElementById(element+"Remove")){
@@ -113,3 +115,29 @@ function addArticleWindow(articleTitle){
   document.getElementById(articleTitle + 'Trigger').click();
 
   }
+
+  document.onkeyup = function(){
+    if(event.key == "Enter" && (document.activeElement === document.getElementById('input_text'))){
+        var tmp = $('.nav-tabs .active').text();
+        var conv = "";
+
+        if (tmp == "") {
+            tmp = Data_Contact[0].Nom;
+        }
+        for (var j = 0; j < Data_Contact.length; j++) {
+            if (Data_Contact[j].Nom == tmp) {
+                conv = Data_Contact[j].Ref;
+            }
+        }
+
+        var ajout = document.getElementById('input_text').value;
+
+        if (ajout != "") {
+            document.getElementById("ajout_message_" + conv).innerHTML +=
+                '<div class="d-flex align-items-end flex-column"><div class="Text_Conv p-2 bd-highlight d-inline-flex px-3 mb-1 text-left bg-white rounded">' + ajout + '</div></div>';
+        }
+
+        ajout = "";
+        document.getElementById('input_text').value = "";
+    }
+};
